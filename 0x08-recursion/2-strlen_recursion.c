f8f2fa03973e8a9d599c36bb9e1637df81d70319
Dercio <12derciomaduna@gmail.com>
2024-01-10 05:15:27 -0800

    I am testing string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+include "main.h"
+
+/**
+ * _strlen_recursion - prints a reversed string,
+ * @s: string to check
+ * Return: void
+ */
+
+void _strlen_recursion(*s)
+{
+if(*s == '\0')
+{
+return 0;
+}
+else
+{
+return 1 + string_length(str + 1);
+}
+}
