69cd4519b9f10821efca9489a8a3b17520ccb16a
Dercio <12derciomaduna@gmail.com>
2024-01-10 04:03:44 -0800

    i a fixed the typo and I am testing the first task.i added a incrementer to continously run the function at the end of it

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,4 @@
-#include main.h
+#include "main.h"
 
 /**
  * _puts_recursion - prints a string, followed by a new line
@@ -10,7 +10,7 @@ void _puts_recursion(char *s)
 {
 if (*s == 0)
 {
-_put_char("\n");
+_putchar("\n");
 }
 _putchar(*s);
 _puts_recursion(s + 1);
