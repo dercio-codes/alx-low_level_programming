bfaf3f46b8f57bbb50d916cd0d7db42e24af448c
Dercio <12derciomaduna@gmail.com>
2024-01-11 01:50:08 -0800

    fixing whitespace

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,1 +1,21 @@
 #include "main.h"
+
+/**
+ * _pow_recursion - returns the power of the given inputs
+ * @y - interget entered for check
+ * @x - interget entered for check
+ */
+
+int _pow_recursion(int x , int y)
+{
+if (x == 0 || y == 0)
+{
+printf("Undefined Entry");
+return (1);
+}
+else
+{
+int answer = (x * x); 
+printf("here is your number : %d", answer);
+}
+}
