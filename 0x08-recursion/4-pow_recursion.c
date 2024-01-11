56a52213645ece4dc2fadd384061dc9eb3d326c1
Dercio <12derciomaduna@gmail.com>
2024-01-11 03:59:35 -0800

    preparing to push for third test, i am checking for negatives numbers now.I I am independetly checking for x and y and handling them accordling.I added parethesis to my return statements

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,18 +11,18 @@ int _pow_recursion(int x, int y)
 {
 if (x == 0 || y == 0)
 {
-return 1;
+return (1);
 }
 else if (y < 0)
 {
-if (x < 0)
+if (x < 0 || y < 0)
 {
-return -1;
+return (-1);
 }
-return 1 / _pow_recursion(x, -y);
+return (1 / _pow_recursion(x, -y));
 }
 else
 {
-return x * _pow_recursion(x, y - 1);
+return (x * _pow_recursion(x, y - 1));
 }
 }
