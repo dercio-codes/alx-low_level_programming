6c9a455162956a58acf8984c6e0b9e2d99f568e7
Dercio <12derciomaduna@gmail.com>
2024-01-12 12:00:07 -0800

    preparing to push the first iteration of test of addition

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - prints its name
+* @argc: argument count
+* @argv: argument array
+* Return: it's name
+*/
+
+int main(int argc, char *argv[])
+{
+int i, total;
+for (i = 0; i < argc ; i++)
+{
+total = total + atoi(argv[i]);
+printf("%s\n", argv[i]);
+}
+return (0);
+}
