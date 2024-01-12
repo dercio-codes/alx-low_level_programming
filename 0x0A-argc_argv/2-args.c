f27641166445e5b3da312ed136657a74e76f380b
Dercio <12derciomaduna@gmail.com>
2024-01-12 10:29:09 -0800

    preparing to push the first iteration of test 1

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - prints its name
+* @argc: argument count
+* @argv: argument array
+* Return: it's name
+*/
+
+int main(int argc, char *argv[])
+{
+int i;
+for(i = 0;i < argc ; i++)
+{
+printf("%s\n",argv[i]);
+}
+return (0);
+}
