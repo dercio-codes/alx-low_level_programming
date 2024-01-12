45fbaf78fac000d5c01353595f82451a051e0d86
Dercio <12derciomaduna@gmail.com>
2024-01-12 04:32:39 -0800

    I changed the array cuz i mispelled it thats why i was getting foo bar error

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - prints file name
+ * @argc: argument count
+ * @argv: argument array
+ * Return: its name
+ */
+
 int main(int argc, char *argv[])
 {
 (void)argc;
