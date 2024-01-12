15f74b30b4004e538a99ad41669b4e5c8bc0f153
Dercio <12derciomaduna@gmail.com>
2024-01-12 04:04:12 -0800

    preparing to test my 2nd functino and check if its working, i might need to add another check to make sure we can return 0 if no paremeters are passed into the function

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,9 +1,10 @@
-#include <stdlib.h>
+#include <stdio.h>
 
-/**
- *
- *
- *
- */
+/**                                                                                                                                                                   * main - prints its name                                                                                                                                             * @argc: argument count                                                                                                                                              * @argv: argument array                                                                                                                                              * Return: it's name                                                                                                                                                  */
 
-int main (int argc , char** argv)
+int main(int argc, char** argv)
+{
+printf("%d\n", argc);
+(void)argv;
+return (0);
+}
