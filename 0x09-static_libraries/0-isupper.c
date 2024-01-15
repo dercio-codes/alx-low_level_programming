0f294b45f4575f9248b6166af8a1121f176b26d1
Dercio <12derciomaduna@gmail.com>
2024-01-15 11:11:39 -0800

    preparing to push my code

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,3 @@
+int isupper( int c) {
+    return 0;
+}
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,7 +13,10 @@ int main(int argc, char *argv[])
 int i, total;
 for (i = 0; i < argc ; i++)
 {
+if (atoi(argv[i]) != 0) 
+{
 total = total + atoi(argv[i]);
+}
 }
 printf("%d\n", total);
 return (0);
