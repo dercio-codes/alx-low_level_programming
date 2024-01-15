b3d4326bc60b2e9953a787e6bb3a8a98546cf365
Dercio <12derciomaduna@gmail.com>
2024-01-15 11:39:15 -0800

    testing my library code

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,5 +1,19 @@
 #include "main.h"
 
-int isupper( int c) {
-    return 0;
+/**
+* _isupper - checks for uppercase character.
+* @c: month in number format
+* Return: 1 if c is uppercase else return 0
+*/
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
