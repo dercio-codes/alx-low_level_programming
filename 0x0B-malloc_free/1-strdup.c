03175a55773dbdf28cc49451ee26929c808c2ba0
Dercio <12derciomaduna@gmail.com>
2024-01-17 02:58:48 -0800

    testig the second task again

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,22 +1,26 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
 
 /**
- *
- *
- *
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: string to allocate
+ * Return: NULL if str = Null else pointer
  */
 
 char *_strdup(char *str)
-
-char strAddress , strDuplicate ;
 {
-  if (str == NULL){
-    return (NULL);
-  }
-  else{
-    strAddress = malloc(sizeof(str));
-    return strAddress;
-  }
+char *strDuplicate;
+
+if (str == NULL)
+{
+return (NULL);
+}
+strDuplicate = malloc(strlen(str) + 1);
+if (strDuplicate == NULL)
+{
+return (NULL);
+}
+strcpy(strDuplicate, str);
+return (strDuplicate);
 }
