b76ed0295089a8016997e1a81d1e8c5fb85a933f
Dercio <12derciomaduna@gmail.com>
2024-01-18 03:12:37 -0800

    refactored the code and now will be testing the other checks

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *concatStr = malloc(strlen(s1) + strlen(s2) + 1);
-if (concatStr == NULL)
+if (concatStr == NULL || s1 == NULL || s2 == NULL || n == NULL)
 {
 return (NULL);
 }
