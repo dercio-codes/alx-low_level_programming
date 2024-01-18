a58f297a470decf565fe79d728665c9856afdeff
Dercio <12derciomaduna@gmail.com>
2024-01-18 04:50:07 -0800

    testing calloc again and checkin if i pass the last 2 check

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,7 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *concatStr = malloc(strlen(s1) + strlen(s2) + 1);
+char *concatStr = malloc(strlen(s1) + n + 1);
 if (concatStr == NULL)
 {
 return (NULL);
