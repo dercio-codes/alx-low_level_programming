a911fa6277f50e4f4f9c4c5e23d87fa737691f1b
Dercio <12derciomaduna@gmail.com>
2024-01-18 02:49:17 -0800

    fixed all betty errors for the second task and now i will e testing again.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,22 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: unknown number of bytes
+ * Return: the new concatenated string
+ */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-void *p = malloc(strlen(s1)+strlen(s2)+1);
-char *concatStr = malloc(strlen(s1) + strlen(s2) + 1 );
+void *p = malloc(strlen(s1) + strlen(s2) + 1);
+char *concatStr = malloc(strlen(s1) + strlen(s2) + 1);
 strcpy(concatStr, s1);
 
-if ( p == NULL )
+if (p == NULL || concatStr == NULL)
 {
-return NULL;
+return (NULL);
 }
 
-if (n < strlen(s2)){
-strncat(concatStr,s2 , n);
-}else{
-strcat(concatStr,s2); 
+if (n < strlen(s2))
+{
+strncat(concatStr, s2, n);
+}
+else
+{
+strcat(concatStr, s2);
 }
 
-return concatStr;
+return (concatStr);
 }
