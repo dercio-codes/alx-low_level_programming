0dd3f9b28c004344717e9268d5a2de7d6c3443d3
Dercio <12derciomaduna@gmail.com>
2024-01-18 04:43:23 -0800

    testing calloc again and checkin if i pass the last 2 check

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,18 +1,16 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 /**
- * _calloc - allocates memory.
- * @nmemb : array of content
- * @size : array size
- * Return: Always 0.
+ * _calloc - Allocates memory for an array of nmemb elements of size bytes
+ * @nmemb : The number of elements in the array.
+ * @size : The size of each element in bytes.
+ * Return: A pointer to the allocated memory, or NULL if nmemb or size is 0
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-char *result = malloc(size * nmemb);
+void *result = malloc(nmemb * size);
+
 if (nmemb == 0 || size == 0)
 {
 return (NULL);
