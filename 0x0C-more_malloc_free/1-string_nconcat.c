33dc29818ebb6498d797cee61371f9df6d99a081
Dercio <12derciomaduna@gmail.com>
2024-01-18 02:59:39 -0800

    refactored the code and now will be testing the other checks

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,8 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "main.h"
 
 /**
  * string_nconcat - concatenates two strings
@@ -12,23 +14,11 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-void *p = malloc(strlen(s1) + strlen(s2) + 1);
 char *concatStr = malloc(strlen(s1) + strlen(s2) + 1);
+if (concatStr == NULL) 
+{
+return NULL;
+}
 strcpy(concatStr, s1);
-
-if (p == NULL || concatStr == NULL)
-{
-return (NULL);
-}
-
-if (n < strlen(s2))
-{
 strncat(concatStr, s2, n);
-}
-else
-{
-strcat(concatStr, s2);
-}
-
-return (concatStr);
-}
+return concatStr;}
