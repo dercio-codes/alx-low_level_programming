d50a0d36ece5c330db384039745a02184129d936
Dercio <12derciomaduna@gmail.com>
2024-01-22 21:31:53 -0800

    created tje first task and added my SIZE macro. and ni also did the second task for PI and added its marco values and Now i created an app that prints he name of the file its compiled to

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints file name
+ * @argc: argument count
+ * @argv: argument array
+ * Return: its name
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
