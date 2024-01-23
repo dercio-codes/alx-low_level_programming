1c2136a99912ef37a647215a58cc4dcc5e04ef69
Dercio <12derciomaduna@gmail.com>
2024-01-22 22:45:04 -0800

    created the init dog function that copies the dog from arguemtns to my struct pointer.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,7 +16,5 @@ char *owner;
 };
 extern void init_dog(struct dog *d, char *name, float age, char *owner);
 extern void print_dog(struct dog *d);
-extern dog_t *new_dog(char *name, float age, char *owner);
-extern void free_dog(dog_t *d);
 
 #endif
