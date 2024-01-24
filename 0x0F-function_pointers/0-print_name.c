e5d1583997f6d593cf0a70698d250d59697d9087
Dercio <12derciomaduna@gmail.com>
2024-01-24 11:48:13 -0800

    preping my pointer function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
+/**
+ * print_name - creates an array of chars
+ * @name: the name we want to print out
+ * @f: a pointer to the function we want top execute
+ * Return: NULL if size = 0 else size
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
-  (*f)(name);
+(*f)(name);
 }
+
+/**
+ * print_the_name - prints the actual name
+ * @name: the name we want to print out
+ */
 
 void print_the_name(char *name)
 {
-  printf("Hello, my name is %s\n", name);
+printf("Hello, my name is %s\n", name);
 }
