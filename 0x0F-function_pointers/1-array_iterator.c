fd2f1296fe41823981e22749f0561ef39ba9e2c4
Dercio <12derciomaduna@gmail.com>
2024-01-25 00:47:39 -0800

    preparing to push my code and test my other code

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,17 +2,19 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - Reallocates a memory block using malloc and free
+ * array_iterator - goes through an array doing what ever the function passed into it does
  * @array: Pointer to the memory previously allocated with a call to malloc
  * @size: Size of the allocated space for ptr
+ * @action: function to execute
  * Return: Pointer to the reallocated memory block
  */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    size_t i;
+size_t i;
 
-    for (i = 0; i < size; i++)
-    {
-        (*action)(array[i]);
-    }
+for (i = 0; i < size; i++)
+{
+(*action)(array[i]);
+}
 }
