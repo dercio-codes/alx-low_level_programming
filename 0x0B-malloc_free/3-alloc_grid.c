87337b6d2552d365bfcd5d753e89d046c3964bc9
Dercio <12derciomaduna@gmail.com>
2024-01-25 11:54:29 -0800

    testing my print strings code

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,11 @@
+
+int **alloc_grid(int width, int height){
+     int i , j ;
+     for ( i = 0 ; i < width ; i ++){
+           for ( j = 0 ; j < height ; j ++){
+         printf("0")
+     }   
+     }
+    return (0);
+    
+}
