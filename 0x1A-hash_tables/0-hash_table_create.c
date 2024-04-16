0c6269ea750baed96a20ba78ccdd7294da817d5c
Dercio <12derciomaduna@gmail.com>
2024-04-16 04:00:42 -0700

    Python classes are cool and fun

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,32 +1,27 @@
-#include <stdlib.h>
+#include "hash_tables.h"
 
-typedef struct hash_table_s
-{
-    unsigned long int size;
-    void **array;
-} hash_table_t;
-
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_table;
+	hash_table_t *ht;
+	unsigned long int i;
 
-    if (size < 1) return NULL; /* invalid size */
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
 
-    /* Attempt to allocate memory for the table structure */
-    if ((new_table = malloc(sizeof(hash_table_t))) == NULL) {
-        return NULL;
-    }
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 
-    /* Attempt to allocate memory for the table itself */
-    if ((new_table->array = malloc(sizeof(void *) * size)) == NULL) {
-        return NULL;
-    }
-
-    /* Initialize the elements of the array */
-    for(unsigned long int i=0; i<size; i++) new_table->array[i] = NULL;
-
-    /* Set the table's size */
-    new_table->size = size;
-
-    return new_table;
+	return (ht);
 }
--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,8 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Get the index at which a key/value pair should
+ *             be stored in array of a hash table.
+ * @key: The key to get the index of.
+ * @size: The size of the array of the hash table.
+ *
+ * Return: The index of the key.
+ *
+ * Description: Uses the djb2 algorithm.
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    /* Use the djb2 hash function to get a hash value for the key */
-    unsigned long int hash = hash_djb2(key);
-
-    /* Modulo by the size to ensure the index is within array bounds */
-    return hash % size;
+	return (hash_djb2(key) % size);
 }
